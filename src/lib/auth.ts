360e04b3b0a8b1382a9d58aad63c67a786e81540
nickasss <nicholaesara16@gmail.com>
2026-05-31 03:48:54 +0800

    feat: update authentication schema and add new account and session tables, enhance user attributes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { admin } from 'better-auth/plugins/admin'
 
 import { db } from '@/db/index.ts'
 
@@ -13,5 +14,5 @@ export const auth = betterAuth({
     enabled: true,
   },
 
-  plugins: [tanstackStartCookies()],
+  plugins: [tanstackStartCookies(), admin()],
 })
